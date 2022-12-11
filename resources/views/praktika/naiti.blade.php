c643ed73aee9d8d49165b1aaac33a74cc7420736
p0ndy <nurkkhaev@mail.ru>
2022-12-12 00:00:56 +0600

    gde nas naiti(statika)

---
+++ resources/views/praktika/naiti.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    
+<div class="container">
+        <div class="d-flex flex-row justify-content-around align-items-center">
+            <img src="map.jpg" alt="map" class="col-lg-6">
+            <div class="d-flex flex-column">
+                <div><b>Наш адрес</b> - ул. Путилова 31</div>
+                <div><b>Наш номер</b> - +7 964 218 64 75</div>
+                <div><b>Наш email</b> - FunnyComicsLand@mail.ru</div>
+            </div>
+        </div>
+    </div>
+@endsection
